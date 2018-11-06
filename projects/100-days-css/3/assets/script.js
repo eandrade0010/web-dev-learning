4cf567efca76fde45a3432ccf65e74282e827854
eandrade0010 <eandrade0010@gmail.com>
2018-11-06 10:17:17 -0800

    completed css challenge

--- projects/100-days-css/3/assets/script.js
+++ projects/100-days-css/3/assets/script.js
@@ -1,6 +1,9 @@
 $( document ).ready(function() {
+    $( ".fa-check").hide();
     $( "a" ).click(function() {
-      
+      $( this ).children("span:last").hide();
+      $( this ).children("span:first").show();
+      $( this ).css("color", "#d3dbdf");
 
   });
 });
