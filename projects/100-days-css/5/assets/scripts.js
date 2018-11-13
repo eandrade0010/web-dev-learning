3f0e082dde778a912e5f9cc314902dd09e33dac9
eandrade0010 <eandrade0010@gmail.com>
2018-11-13 15:48:50 -0800

    another css challenge done

---
+++ projects/100-days-css/5/assets/scripts.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+    $( ".container" ).click(function() {
+      $(".glow").css("visibility", "hidden");
+      $(".bulb, .stem").css("background-color", "#B0A7A8");
+  });
+});
