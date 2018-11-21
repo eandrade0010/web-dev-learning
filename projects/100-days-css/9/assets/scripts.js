811798e599db7c7ecfcdcc42c1d0f7efad132fa7
eandrade0010 <eandrade0010@gmail.com>
2018-11-21 10:12:56 -0800

    finished last css challenge

--- projects/100-days-css/9/assets/scripts.js
+++ projects/100-days-css/9/assets/scripts.js
@@ -1,29 +1,29 @@
-function dragger(element) {
-  $(document).on('mousemove', function(e) {
-    element.css({
-      left: e.pageX
-    });
+function holdNDrag(element) {
+  limit = element.parent().width();
+  frameWidth = $(".frame").width();
+  // console.log(elemParent);
+  mouseup = false;
+  $(document).on('mouseup', function() {
+    mouseup = true;
+    return;
   });
-}
+  $(document).on('mousemove', function(e) {
+    if (!mouseup) {
+
+      if (e.pageX > limit/4 && e.pageX < (frameWidth - limit/4)) {
+        element.css({
+          left: e.pageX - limit/4 - element.width()/2
+        });
+
+      }
+    };
+  });
+};
 
 
 $(document).ready(function() {
-
   var dragElem = $(".centered-circle");
-  var listener = 0;
   dragElem.on('mousedown', function() {
-    console.log("hello");
-
-    listener = setTimeout(
-      dragger(dragElem), 1000);
+    holdNDrag(dragElem);
   });
-  dragElem.on('mouseup mouseleave', function() {
-    console.log("wat");
-    clearTimeout(listener);
-
-  });
-
-
-
-
 })
