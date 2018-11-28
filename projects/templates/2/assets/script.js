8c568b9b8f5a75c1fd81ff38ef0f3406f9a1ae57
eandrade0010 <eandrade0010@gmail.com>
2018-11-27 17:34:29 -0800

    new template project

---
+++ projects/templates/2/assets/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  console.log(screen.width);
+
+  // setInterval(function() {
+  //   width = $(window).width();
+  //   console.log(width);
+  //   if (width <= 50%) {
+  //     $(".image-text").css("font-size", "60px");
+  //       // font-size = font-size - 1
+  //     };
+  //   if (width <= 350) {
+  //     $("#home-img").css("visibility", "hidden");
+  //     };
+  //
+  //
+  //
+  //
+  //
+  // }, 500);
+
+  // width = $(window).width();
+  // console.log(width);
+
+
+
+})
