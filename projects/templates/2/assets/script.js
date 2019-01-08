156a19435af28cb0d426280897f451058189fac9
eandrade0010 <eandrade0010@gmail.com>
2019-01-07 17:36:30 -0800

    cs50 course!

--- projects/templates/2/assets/script.js
+++ projects/templates/2/assets/script.js
@@ -1,26 +1,34 @@
+
+
 $(document).ready(function() {
-  console.log(screen.width);
 
-  // setInterval(function() {
-  //   width = $(window).width();
-  //   console.log(width);
-  //   if (width <= 50%) {
-  //     $(".image-text").css("font-size", "60px");
-  //       // font-size = font-size - 1
-  //     };
-  //   if (width <= 350) {
-  //     $("#home-img").css("visibility", "hidden");
-  //     };
-  //
-  //
-  //
-  //
-  //
-  // }, 500);
+  var scroll = 0;
+  console.log(scroll);
+  var lastScrollTop = 0;
+  var elements = [1,2,3,4,5];
+  var i = 0;
+  $( ".main-content" ).scroll(function() {
+    var st = $(this).scrollTop();
+    if (st > lastScrollTop) {
+      i++;
+      console.log('down');
+      if (i >= elements.length ) {
+        i = 0;
+      }
+      console.log(i);
 
-  // width = $(window).width();
-  // console.log(width);
+    } else {
+      console.log('up');
+    }
+    lastScrollTop = st;
+    //
+    // scroll = $(window).scrollTop();
+    // console.log(scroll);
+    $(".main-content").scrollTop($(".breh").offset().top);
+    $(".main-content").off("scroll");
+  });
 
 
 
-})
+
+});
